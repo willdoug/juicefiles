669829b04a28a2c04b53f4a54c7d55d7977e0ba3
William Castro <williamweb2.0@gmail.com>
2023-07-25 11:08:37 -0300

    Create juicefile.sh

---
+++ juicefile.sh
@@ -0,0 +1,12 @@
+#simple script to combine file wordlists x extensions wordlist to make new wordlists to get juice files.
+#!/bin/bash
+
+for file in $(cat /usr/share/wordlists/seclists/Discovery/Web-Content/raft-small-directories-lowercase.txt)
+do
+for extension in $(cat /usr/share/wordlists/seclists/Discovery/Web-Content/raft-medium-extensions-lowercase.txt)
+do
+echo "$file$extension" > new_wordlist.txt
+echo "$file$extension"
+done
+done
+
